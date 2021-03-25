ad86b61990c5efc0af92b33c4f7b0e858067f109
dasolovkin <dasolovkinne@mail.ru>
2021-03-26 02:32:45 +0300

    Домашняя работа №4

--- 4-services/1-home-page/src/app/home-page/home-page.component.ts
+++ 4-services/1-home-page/src/app/home-page/home-page.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'ngx-shop-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.sass'],
 })
 // @ts-ignore
-export class HomePageComponent {}
+export class HomePageComponent {
+  @Input() public categories: any = null;
+  @Input() public products: any = null;
+
+  public goToProduct() {
+    
+  }
+  public goToBasket() {
+
+  }
+
+  public redirectTo(subCategory: string) {
+
+  }  
+}
